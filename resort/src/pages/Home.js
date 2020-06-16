2321a17764080edf18d56aec1f0c93a461067b61
Samsoucy <newf930@outlook.com>
2020-06-15 20:14:21 -0400

    added hero component

--- resort/src/pages/Home.js
+++ resort/src/pages/Home.js
@@ -1,10 +1,7 @@
 import React from 'react'
+import Hero from "../components/Hero"
 
 export default function Home() {
-    return (
-        <div>
-            hellow from home page
-        </div>
-    )
+    return <Hero hero="defaultHero" />;
 }
 
