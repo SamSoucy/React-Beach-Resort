c387f0fc3b0d2a2e7d4748fdc2b683b146bf01f5
Samsoucy <newf930@outlook.com>
2020-06-16 21:24:42 -0400

    added loading and romm components

--- resort/src/components/FeaturedRooms.js
+++ resort/src/components/FeaturedRooms.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
 import { RoomContext } from "../context";
-
+import Loading from "./Loading";
+import Room from "./Room";
+import Title from "./Title";
+    
 export default class FeaturedRooms extends Component {
     static contextType = RoomContext;
     render() {
-        return <div>from featured Rooms</div>;
+        const { loading, featuredRooms: rooms } = this.context;
+        
+        return (
+            <div>
+                from featured Rooms
+                 <Room/>
+                 <Loading/>
+            </div>
+
+        );
     }
 }
