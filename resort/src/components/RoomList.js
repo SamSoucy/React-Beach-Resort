baac0fc65a51b481ba8d7ac05400e7440f1eafa8
Samsoucy <newf930@outlook.com>
2020-06-17 21:33:41 -0400

    built out room list and rendered it

--- resort/src/components/RoomList.js
+++ resort/src/components/RoomList.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import Room from "./Room";
 
-export default function RoomsList() {
+export default function RoomsList({ rooms }) {
+    if (rooms.length === 0) {
+        return (
+            <div className="empty-search">
+                <h3>unfortunatly no rooms match your search parameters</h3>
+            </div>
+        )
+    }
     return (
-        <div>
-            hello from RoomsList
-        </div>
-    )
+        <section className="roomslist">
+            <div className="roomslist-center">
+                {rooms.map(item => {
+                    return <Room key={item.id} room={item} />;
+                })}
+            </div>
+        </section>
+    );
 }
