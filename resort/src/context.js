071caa7cd90f8a1cfa87b1bedb473ba0fc2f1e64
Samsoucy <newf930@outlook.com>
2020-06-27 13:52:00 -0400

    switched form local data to external data with contentful also set up env file

--- resort/src/context.js
+++ resort/src/context.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
-import items from "./data";
+// import items from "./data";
+import Client from "./Contentful";
+
+
 
 const RoomContext = React.createContext();
 //<RoomContext.Provider value={"hello"}
@@ -21,22 +24,34 @@ class RoomProvider extends Component {
         pets: false
     };
     //getData
+    getData = async () => {
+        try {
+            let response = await Client.getEntries({
+                content_type: "reactBeachResortProject",
+                order: 'sys.createdAt'
+            });
+            let rooms = this.formatData(response.items);
+            let featuredRooms = rooms.filter(room => room.featured === true);
+            let maxPrice = Math.max(...rooms.map(item => item.price));
+            let maxSize = Math.max(...rooms.map(item => item.size));
+            this.setState({
+                rooms,
+                featuredRooms,
+                sortedRooms: rooms,
+                loading: false,
+                price: maxPrice,
+                maxPrice,
+                maxSize
+            });
+        } catch (error) {
+            console.log(error);
+            
+        }
+    };
 
     componentDidMount() {
-        //this.getData
-        let rooms = this.formatData(items);
-        let featuredRooms = rooms.filter(room => room.featured === true);
-        let maxPrice = Math.max(...rooms.map(item => item.price));
-        let maxSize = Math.max(...rooms.map(item => item.size));
-        this.setState({
-            rooms,
-            featuredRooms,
-            sortedRooms: rooms,
-            loading: false,
-            price: maxPrice,
-            maxPrice,
-            maxSize
-        });
+        this.getData();
+        
     }
 
     formatData(items) {
